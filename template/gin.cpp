3d77c807c5fef1e2650ab1a96189ea476dc2b3ee
jeffry1829 <jeffry1829@gmail.com>
2020-08-18 15:49:46 +0800

    add template

--- template/gin.cpp
+++ template/gin.cpp
@@ -5,6 +5,16 @@ inline bool gin(int &x) {
   while (c = getchar(), c < '0' || c > '9')
     if (c == -1) return 0;
   x = c - '0';
-  while (c = getchar(), c >= '0' && c <= '9') x = x * 10 + c - '0';
+  while (c = getchar(), c >= '0' && c <= '9') x = (x<<3) + (x<<1) + c - '0';
+  return 1;
+}
+
+// Can't use in codeforce
+inline bool gin(int &x) {
+  char c;
+  while (c = getchar_unlocked(), c < '0' || c > '9')
+    if (c == -1) return 0;
+  x = c - '0';
+  while (c = getchar_unlocked(), c >= '0' && c <= '9') x = (x<<3) + (x<<1) + c - '0';
   return 1;
 }
