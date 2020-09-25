75ffd9f25cd223213e402d0516ea0a2794a08c9f
jeffry1829 <jeffry1829@gmail.com>
2020-09-25 12:35:16 +0800

    1420D

--- 1420D.cpp
+++ 1420D.cpp
@@ -62,7 +62,7 @@ typedef long long ll;
 typedef pair<int,int> PII;
 typedef double db;
 mt19937 mrand(random_device{}());
-const ll mod=1000000007;
+const ll mod=998244353;
 int rnd(int x){return mrand()%x;}
 ll powmod(ll a,ll b){ll res=1;a%=mod;assert(b>=0);for(;b;b>>=1){if(b&1)res=res*a%mod;a=a*a%mod;}return res;}
 ll gcd(ll a, ll b){return b?gcd(b,a%b):a;}
@@ -72,10 +72,33 @@ ll gcd(ll a, ll b){return b?gcd(b,a%b):a;}
 #define left aijhgpiaejhgp
 //#define end aononcncnccc
 inline int pmod(int x, int d){int m = x%d;return m+((m>>31)&d);}
+#define la kaofjpfo
 //head
-const int _n=3e5+10;
-int t,n,k,l[_n],r[_n];
+const int _n=3e5+10,MAXB=21;
+int t,n,k,a,b;
+PII la[_n];
+int fac[_n],inv[_n];
+void exgcd(int a,int b,int& d,int& x,int& y){
+  if(!b)x=1,y=0,d=a;
+  else exgcd(b,a%b,d,y,x),y-=x*(a/b);
+}
+int C(int m,int n){
+  if(m<n)return 0;
+  if(m<mod and n<mod)return 1ll*fac[m]*inv[n]%mod*inv[m-n]%mod;
+  return 1ll*C(m/mod,n/mod)*C(m%mod,n%mod)%mod;
+}
+void genInv(){
+  fac[0]=1;rep(i,1,n+1)fac[i]=1ll*fac[i-1]*i%mod;
+  //int tmp1,tmp2;rep(i,0,n+1)exgcd(fac[i],mod,tmp1,inv[i],tmp2);
+  rep(i,0,n+1)inv[i]=powmod(fac[i],mod-2);
+}
 main(void) {ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-  cin>>n>>k;
+  cin>>n>>k;rep(i,0,n){cin>>a>>b; la[i].fi=a,la[i].se=b;} 
+  sort(la,la+n); genInv(); ll ans=0;
+  per(i,0,n){
+    int res1=0;per(j,0,MAXB)if(res1+(1<<j)<i and la[res1+(1<<j)].se<la[i].fi)res1+=(1<<j);
+    t=i-res1;
+    ans+=C(t,k-1),ans%=mod;
+  }cout<<ans<<'\n';
   return 0;
 }
--- template/exgcd(others).cpp
+++ template/exgcd(others).cpp
@@ -1,8 +1,4 @@
-void exgcd(int a, int b, int& d, int& x, int& y){
-    if(!b){
-        x=1, y=0, d=a;
-    }else{
-        exgcd(b, a%b, d, y, x);
-        y-=x*(a/b);
-    }
+void exgcd(int a,int b,int& d,int& x,int& y){
+    if(!b)x=1,y=0,d=a;
+    else exgcd(b, a%b, d, y, x),y-=x*(a/b);
 }
