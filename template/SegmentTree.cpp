bf970349f1dcb54c1ded3727de2aa0135a742ba7
jeffry1829 <jeffry1829@gmail.com>
2025-03-11 13:36:15 +0800

    a

--- template/SegmentTree.cpp
+++ template/SegmentTree.cpp
@@ -1,5 +1,5 @@
-//#pragma GCC optimize(1)
-//#pragma GCC optimize(2)
+// #pragma GCC optimize(1)
+// #pragma GCC optimize(2)
 #pragma GCC optimize(3)
 #pragma GCC optimize("Ofast")
 #pragma GCC optimize("inline")
@@ -44,82 +44,227 @@
 #pragma GCC optimize("-fdelete-null-pointer-checks")
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i,a,n) for(int i=a;i<n;i++)
-#define per(i,a,n) for(int i=n-1;i>=a;i--)
+#define rep(i, a, n) for (int i = a; i < n; i++)
+#define per(i, a, n) for (int i = n - 1; i >= a; i--)
 #define pb push_back
-//#define mp make_pair
-#define all(x) (x).begin(),(x).end()
+// #define mp make_pair
+#define all(x) (x).begin(), (x).end()
 #define fi first
 #define se second
 #define SZ(x) ((int)(x).size())
-#define min(a,b) (((a)<(b))?(a):(b))
-#define max(a,b) (((a)>(b))?(a):(b))
-#define abs(x) (((x)<0)?(-(x)):(x))
+#define min(a, b) (((a) < (b)) ? (a) : (b))
+#define max(a, b) (((a) > (b)) ? (a) : (b))
+#define abs(x) (((x) < 0) ? (-(x)) : (x))
 typedef vector<int> VI;
 typedef long long ll;
-typedef pair<int,int> PII;
+typedef pair<int, int> PII;
 typedef double db;
 mt19937 mrand(random_device{}());
-const ll mod=1000000007;
-int rnd(int x){return mrand()%x;}
-ll powmod(ll a,ll b){ll res=1;a%=mod;assert(b>=0);for(;b;b>>=1){if(b&1)res=res*a%mod;a=a*a%mod;}return res;}
-ll gcd(ll a, ll b){return b?gcd(b,a%b):a;}
+const ll mod = 1000000007;
+int rnd(int x) { return mrand() % x; }
+ll powmod(ll a, ll b) {
+  ll res = 1;
+  a %= mod;
+  assert(b >= 0);
+  for (; b; b >>= 1) {
+    if (b & 1) res = res * a % mod;
+    a = a * a % mod;
+  }
+  return res;
+}
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
 #define rank oiajgpowsdjg
 const int N = 100;
 int parent[N], rank[N];
-inline void dsinit(int n) {for (int i = 0; i < n; i++)parent[i] = i;memset(rank, 0, sizeof rank);}
-inline int dsfind(int e) {return parent[e] == e ? e : parent[e] = dsfind(parent[e]);}
-inline void dsunion(int s1, int s2) {if (rank[s1] < rank[s2])swap(s1, s2);parent[s2] = s1;if (rank[s1] == rank[s2]) rank[s1]++;}
+inline void dsinit(int n) {
+  for (int i = 0; i < n; i++) parent[i] = i;
+  memset(rank, 0, sizeof rank);
+}
+inline int dsfind(int e) { return parent[e] == e ? e : parent[e] = dsfind(parent[e]); }
+inline void dsunion(int s1, int s2) {
+  if (rank[s1] < rank[s2]) swap(s1, s2);
+  parent[s2] = s1;
+  if (rank[s1] == rank[s2]) rank[s1]++;
+}
 #define y1 ojsapogjahg
 #define prev ojaposjdas
-//#define end aononcncnccc
-inline int pmod(int x, int divisor){int m = x % divisor;return m + ((m >> 31) & divisor);}
-//head
-const int _n=2e5+10;
+// #define end aononcncnccc
+inline int pmod(int x, int divisor) {
+  int m = x % divisor;
+  return m + ((m >> 31) & divisor);
+}
+// head
+const int _n = 2e5 + 10;
 int n;
 ll s[_n];
-namespace Seg{
-  int nn;
-  ll t[_n<<2],laz[_n<<2];
-  void pull(int v){t[v]=min(t[2*v+1],t[2*v+2]);}
-  void apply(int v, ll val){t[v]+=val,laz[v]+=val;}
-  void push(int v){
-    if(laz[v]!=0)apply(2*v+1,laz[v]),apply(2*v+2,laz[v]),laz[v]=0;
-  }
-  void build(int v, int l, int r){
-    if(l+1==r)t[v]=s[l];
-    else{int m=(l+r)>>1;build(2*v+1,l,m),build(2*v+2,m,r);pull(v);}
-  }
-  void add(int v,int l,int r,int ql,int qr,ll val){
-    if(r<=ql or qr<=l)return;
-    else if(ql<=l and r<=qr)apply(v,val);
-    else{
-      push(v);int m=(l+r)>>1;
-      add(2*v+1,l,m,ql,qr,val),add(2*v+2,m,r,ql,qr,val);
-      pull(v);
-    }
-  }
-  void add(int l,int r,ll val){add(0,0,nn,l,r,val);}
-  void add(int pos,ll val){add(0,0,nn,pos,pos+1,val);}
-  void init(int n_){nn=n_;build(0,0,nn);}
-  int lz(int v,int l,int r){
-    int m=(l+r)>>1;if(l+1==r)return l;
-    int res;push(v);
-    if(t[2*v+2]==0)res=lz(2*v+2,m,r);
-    else res=lz(2*v+1,l,m);
-    pull(v);return res;
-  }
-  int query(int v,int l,int r,int ql,int qr){ //懶標記未確認運作
-    if(r<=ql or l>=qr)return 1e9+100;
-    if(ql<=l and qr>=r)return t[v];
-    int m=(l+r)>>1,res;push(v);
-    res=min(query(2*v+1,l,m,ql,qr),query(2*v+2,m,r,ql,qr));
-    pull(v);return res;
+namespace Seg {
+int nn;
+ll t[_n << 2], laz[_n << 2];
+void pull(int v) { t[v] = min(t[2 * v + 1], t[2 * v + 2]); }
+void apply(int v, ll val) { t[v] += val, laz[v] += val; }
+void push(int v) {
+  if (laz[v] != 0) apply(2 * v + 1, laz[v]), apply(2 * v + 2, laz[v]), laz[v] = 0;
+}
+void build(int v, int l, int r) {
+  if (l + 1 == r)
+    t[v] = s[l];
+  else {
+    int m = (l + r) >> 1;
+    build(2 * v + 1, l, m), build(2 * v + 2, m, r);
+    pull(v);
   }
 }
-main(void) {cin.tie(0);ios_base::sync_with_stdio(0);
+void add(int v, int l, int r, int ql, int qr, ll val) {
+  if (r <= ql or qr <= l)
+    return;
+  else if (ql <= l and r <= qr)
+    apply(v, val);
+  else {
+    push(v);
+    int m = (l + r) >> 1;
+    add(2 * v + 1, l, m, ql, qr, val), add(2 * v + 2, m, r, ql, qr, val);
+    pull(v);
+  }
+}
+void add(int l, int r, ll val) { add(0, 0, nn, l, r, val); }
+void add(int pos, ll val) { add(0, 0, nn, pos, pos + 1, val); }
+void init(int n_) {
+  nn = n_;
+  build(0, 0, nn);
+}
+int lz(int v, int l, int r) {
+  int m = (l + r) >> 1;
+  if (l + 1 == r) return l;
+  int res;
+  push(v);
+  if (t[2 * v + 2] == 0)
+    res = lz(2 * v + 2, m, r);
+  else
+    res = lz(2 * v + 1, l, m);
+  pull(v);
+  return res;
+}
+int query(int v, int l, int r, int ql, int qr) {  // 懶標記未確認運作
+  if (r <= ql or l >= qr) return 1e9 + 100;
+  if (ql <= l and qr >= r) return t[v];
+  int m = (l + r) >> 1, res;
+  push(v);
+  res = min(query(2 * v + 1, l, m, ql, qr), query(2 * v + 2, m, r, ql, qr));
+  pull(v);
+  return res;
+}
+}  // namespace Seg
+main(void) {
+  cin.tie(0);
+  ios_base::sync_with_stdio(0);
 
   return 0;
 }
-//改成class可以一次宣告很多個，只要把namespace改上class，加上public:
-//然後在namespace結尾的}加上分號即可。
+// 改成class可以一次宣告很多個，只要把namespace改上class，加上public:
+// 然後在namespace結尾的}加上分號即可。
+// 上面是min的版本
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// 這是max的版本
+// 837. Sliding Window Maximum
+using ll = long long;
+const int _n = 1e5 + 10;
+int n;
+vector<int> s;
+namespace Seg {
+int nn;
+ll t[_n << 2], laz[_n << 2];
+void pull(int v) { t[v] = max(t[2 * v + 1], t[2 * v + 2]); }
+void apply(int v, ll val) { t[v] += val, laz[v] += val; }
+void push(int v) {
+  if (laz[v] != 0) apply(2 * v + 1, laz[v]), apply(2 * v + 2, laz[v]), laz[v] = 0;
+}
+void build(int v, int l, int r) {
+  if (l + 1 == r)
+    t[v] = s[l];
+  else {
+    int m = (l + r) >> 1;
+    build(2 * v + 1, l, m), build(2 * v + 2, m, r);
+    pull(v);
+  }
+}
+void add(int v, int l, int r, int ql, int qr, ll val) {
+  if (r <= ql or qr <= l)
+    return;
+  else if (ql <= l and r <= qr)
+    apply(v, val);
+  else {
+    push(v);
+    int m = (l + r) >> 1;
+    add(2 * v + 1, l, m, ql, qr, val), add(2 * v + 2, m, r, ql, qr, val);
+    pull(v);
+  }
+}
+void add(int l, int r, ll val) { add(0, 0, nn, l, r, val); }
+void add(int pos, ll val) { add(0, 0, nn, pos, pos + 1, val); }
+void init(int n_) {
+  nn = n_;
+  build(0, 0, nn);
+}
+int lz(int v, int l, int r) {
+  int m = (l + r) >> 1;
+  if (l + 1 == r) return l;
+  int res;
+  push(v);
+  if (t[2 * v + 2] == 0)
+    res = lz(2 * v + 2, m, r);
+  else
+    res = lz(2 * v + 1, l, m);
+  pull(v);
+  return res;
+}
+int query(int v, int l, int r, int ql, int qr) {  // 懶標記未確認運作
+  if (r <= ql or l >= qr) return -1e9;
+  if (ql <= l and qr >= r) return t[v];
+  int m = (l + r) >> 1, res;
+  push(v);
+  res = max(query(2 * v + 1, l, m, ql, qr), query(2 * v + 2, m, r, ql, qr));
+  pull(v);
+  return res;
+}
+}  // namespace Seg
+class Solution {
+ public:
+  vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+    s = nums;
+    n = nums.size();
+    Seg::init(nums.size());
+    vector<int> ans;
+    for (int i = k - 1; i < nums.size(); i++) {
+      ans.push_back(Seg::query(0, 0, n, i - k + 1, i + 1));
+    }
+    return ans;
+  }
+};
--- template/dijkstra.cpp
+++ template/dijkstra.cpp
@@ -1,8 +1,8 @@
 /*
-* 注意!這個Template並不保證在任何情況下都work
-*/
-//#pragma GCC optimize(1)
-//#pragma GCC optimize(2)
+ * 注意!這個Template並不保證在任何情況下都work
+ */
+// #pragma GCC optimize(1)
+// #pragma GCC optimize(2)
 #pragma GCC optimize(3)
 #pragma GCC optimize("Ofast")
 #pragma GCC optimize("inline")
@@ -48,68 +48,132 @@
 #pragma comment(linker, "/STACK:1024000000,1024000000")
 #include <bits/stdc++.h>
 using namespace std;
-//#define int long long
-#define rep(i,a,n) for(int i=a;i<n;i++)
-#define per(i,a,n) for(int i=n-1;i>=a;i--)
+// #define int long long
+#define rep(i, a, n) for (int i = a; i < n; i++)
+#define per(i, a, n) for (int i = n - 1; i >= a; i--)
 #define pb push_back
-//#define mp make_pair
-#define all(x) (x).begin(),(x).end()
+// #define mp make_pair
+#define all(x) (x).begin(), (x).end()
 #define fi first
 #define se second
 #define SZ(x) ((int)(x).size())
-#define min(a,b) (((a)<(b))?(a):(b))
-#define max(a,b) (((a)>(b))?(a):(b))
-#define abs(x) (((x)<0)?(-(x)):(x))
+#define min(a, b) (((a) < (b)) ? (a) : (b))
+#define max(a, b) (((a) > (b)) ? (a) : (b))
+#define abs(x) (((x) < 0) ? (-(x)) : (x))
 typedef vector<int> VI;
 typedef long long ll;
-typedef pair<int,int> PII;
+typedef pair<int, int> PII;
 typedef double db;
 mt19937 mrand(random_device{}());
-const ll mod=1000000007;
-int rnd(int x){return mrand()%x;}
-ll powmod(ll a,ll b){ll res=1;a%=mod;assert(b>=0);for(;b;b>>=1){if(b&1)res=res*a%mod;a=a*a%mod;}return res;}
-ll gcd(ll a, ll b){return b?gcd(b,a%b):a;}
-inline int pmod(int x, int d){int m = x%d;return m+((m>>31)&d);}
+const ll mod = 1000000007;
+int rnd(int x) { return mrand() % x; }
+ll powmod(ll a, ll b) {
+  ll res = 1;
+  a %= mod;
+  assert(b >= 0);
+  for (; b; b >>= 1) {
+    if (b & 1) res = res * a % mod;
+    a = a * a % mod;
+  }
+  return res;
+}
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+inline int pmod(int x, int d) {
+  int m = x % d;
+  return m + ((m >> 31) & d);
+}
 #define y1 ojsapogjahg
 #define prev ojaposjdas
 #define rank oiajgpowsdjg
 #define left aijhgpiaejhgp
-//#define end aononcncnccc
-//head
-const int _n=1e5+10;
-const ll inf=1e18;
-int t,n,m,k,u,v,x,s,y,ans,train[_n];
+// #define end aononcncnccc
+// head
+const int _n = 1e5 + 10;
+const ll inf = 1e18;
+int t, n, m, k, u, v, x, s, y, ans, train[_n];
 ll dis[_n];
-struct WE{int to,w;};
-struct W{ll f,t,w;
-bool operator<(const W& rhs)const{return w>rhs.w;}};
+struct WE {
+  int to, w;
+};
+struct W {
+  ll f, t, w;
+  bool operator<(const W& rhs) const { return w > rhs.w; }
+};
 vector<WE> G[_n];
 priority_queue<W> pq;
-main(void) {ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-  cin>>n>>m>>k;rep(i,0,m){cin>>u>>v>>x;G[u].pb({v,x}),G[v].pb({u,x});}
-  rep(i,0,k){
-    cin>>s>>y;
-    if(!train[s])train[s]=y;
-    else if(train[s])ans++,train[s]=min(train[s],y);
+main(void) {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  cin >> n >> m >> k;
+  rep(i, 0, m) {
+    cin >> u >> v >> x;
+    G[u].pb({v, x}), G[v].pb({u, x});
   }
-  
-  
-  //START
-  //THIS TEMPLATE IS FOR REFERENCE PURPOSE
-  rep(i,1,n+1)dis[i]=inf;
-  rep(i,2,n+1)if(train[i])pq.push({0,i,train[i]});
-  pq.push({0,1,0});while(!pq.empty()){
-    W now=pq.top();pq.pop();if(now.w>=dis[now.t])continue;
-    dis[now.t]=now.w;
-    for(WE& u:G[now.t]){
-      if(dis[now.t]+u.w<dis[u.to])pq.push({now.t,u.to,dis[now.t]+u.w});
-      if(train[u.to] and dis[now.t]+u.w<=train[u.to])ans++,train[u.to]=0;
+  rep(i, 0, k) {
+    cin >> s >> y;
+    if (!train[s])
+      train[s] = y;
+    else if (train[s])
+      ans++, train[s] = min(train[s], y);
+  }
+
+  // START
+  // THIS TEMPLATE IS FOR REFERENCE PURPOSE
+  rep(i, 1, n + 1) dis[i] = inf;
+  rep(i, 2, n + 1) if (train[i]) pq.push({0, i, train[i]});
+  pq.push({0, 1, 0});
+  while (!pq.empty()) {
+    W now = pq.top();
+    pq.pop();
+    if (now.w >= dis[now.t]) continue;
+    dis[now.t] = now.w;
+    for (WE& u : G[now.t]) {
+      if (dis[now.t] + u.w < dis[u.to]) pq.push({now.t, u.to, dis[now.t] + u.w});
+      if (train[u.to] and dis[now.t] + u.w <= train[u.to]) ans++, train[u.to] = 0;
     }
   }
-  
-  
-  
-  
-  cout<<ans<<'\n';
+
+  cout << ans << '\n';
   return 0;
 }
+
+/////////////////////////////////////
+struct E {
+  int x1, y1, x2, y2, w;
+  bool operator<(auto& rhs) const {
+    return w > rhs.w;
+  }
+};
+class Solution {
+ public:
+  int dis[110][110], n, m;
+  bool vis[110][110];
+  int minCost(vector<vector<int>>& grid) {
+    n = grid.size();
+    m = grid[0].size();
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        dis[i][j] = 0x7f7f7f7f;
+      }
+    }
+    dis[0][0] = 0;
+    priority_queue<E> pq;
+    pq.push({0, 0, 0, 0, 0});
+    while (!pq.empty()) {
+      E curE = pq.top();
+      pq.pop();
+      if (vis[curE.x2][curE.y2]) continue;  // 其實只要判斷有沒有已經在最短路徑樹上就好
+      dis[curE.x2][curE.y2] = curE.w;
+      int x = curE.x2, y = curE.y2;
+      // 判斷dis是不是比較小其實不需要 感覺只是常數優化
+      if (y + 1 < m and !vis[x][y + 1] and dis[x][y + 1] > dis[x][y] + (grid[x][y] == 1 ? 0 : 1)) pq.push({x, y, x, y + 1, dis[x][y] + (grid[x][y] == 1 ? 0 : 1)});
+      if (y - 1 >= 0 and !vis[x][y - 1] and dis[x][y - 1] > dis[x][y] + (grid[x][y] == 2 ? 0 : 1)) pq.push({x, y, x, y - 1, dis[x][y] + (grid[x][y] == 2 ? 0 : 1)});
+      if (x + 1 < n and !vis[x + 1][y] and dis[x + 1][y] > dis[x][y] + (grid[x][y] == 3 ? 0 : 1)) pq.push({x, y, x + 1, y, dis[x][y] + (grid[x][y] == 3 ? 0 : 1)});
+      if (x - 1 >= 0 and !vis[x - 1][y] and dis[x - 1][y] > dis[x][y] + (grid[x][y] == 4 ? 0 : 1)) pq.push({x, y, x - 1, y, dis[x][y] + (grid[x][y] == 4 ? 0 : 1)});
+
+      vis[x][y] = 1;
+    }
+    return dis[n - 1][m - 1];
+  }
+};
